20978eb14a1bb98db3b49bceb775bbe5495007c9
Frost1738 <frostLilbeng@gmail.com>
2026-01-26 10:29:59 +0300

    experience

--- app/therapy/experiences/page.js
+++ app/therapy/experiences/page.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Experience from "./experience";
 import { getReviews } from "@/app/ApiServices/getFunctions";
+import { createSupabaseServerClient } from "../supabase/server";
 
 export default async function Page() {
   const data = await getReviews();
+  const supabase = await createSupabaseServerClient();
+  const { data: sessionData } = await supabase.auth.getSession();
+  const display_name = sessionData?.session?.user?.user_metadata?.display_name;
 
   return (
     <div className="h-[auto] min-h-[1000px] w-[100%] bg-[#92817A]  md:pt-[1rem] md:pl-[2rem]">
@@ -15,6 +19,7 @@ export default async function Page() {
             name={review.name}
             massageTitle={review.massageTitle}
             key={index}
+            clientName={display_name}
           />
         ))}
       </ul>
